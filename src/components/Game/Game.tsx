e380d23ab2fccef4a09e9ea731ad8b54f57efd63
DavidDemory <david.demory0@gmail.com>
2023-11-24 17:29:04 +0100

    There is now a playableJamJar

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,9 +1,11 @@
 import { GameContainer } from './Game.styles.ts';
 import JamJar from '../JamJar/JamJar.tsx';
+import PlayableJamJar from '../PlayableJamJar/PlayableJamJar.tsx';
 
 export default function Game() {
   return (
     <GameContainer>
+      <PlayableJamJar/>
       <JamJar position={{ x: 50, y: 50 }} color={'white'} />
     </GameContainer>
   );
